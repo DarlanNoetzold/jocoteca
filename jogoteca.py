879b1b88df48c72127520b937697484eff07b21d
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-01-08 08:15:20 -0300

    .

--- jogoteca.py
+++ jogoteca.py
@@ -9,7 +9,7 @@ app.secret_key = 'alura'
 
 app.config['MYSQL_HOST'] = "localhost"
 app.config['MYSQL_USER'] = "root"
-app.config['MYSQL_PASSWORD'] = "admin"
+app.config['MYSQL_PASSWORD'] = "root"
 app.config['MYSQL_DB'] = "jogoteca"
 app.config['MYSQL_PORT'] = 3306
 
